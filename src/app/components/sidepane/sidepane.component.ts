c086170d87a1674d161dd9742bce5d84be7bdb61
phenom_abdul_gaffar <abdul.gaffar@phenompeople.com>
2018-02-18 21:58:26 +0530

    Refactoring code from bootstrap to angular material

---
+++ src/app/components/sidepane/sidepane.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'gem-sidepane',
+  templateUrl: './sidepane.component.html',
+  styleUrls: ['./sidepane.component.scss']
+})
+export class SidepaneComponent implements OnInit {
+
+ @Input() title: string;
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
